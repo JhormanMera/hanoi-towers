fb95f41d05157371ef7779e93352a3d32f7ffaa2
JhormanMera <jhorman.mera1@correo.icesi.edu.co>
2021-04-14 18:19:43 -0500

    hanoi 1
    first version of the recursive method to solve the hanoi problem

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,36 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Main {
-
+	private static Scanner sc =new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int cases = sc.nextInt();
+		sc.nextLine();
+		int repeat=0;
+		do {
+			repeat++;
+			
+		}while(repeat)
+	}
+	private static void hanoiTowers(int d, int t1, int t2, int t3) {
+		String space=" ";
+		if(d==1){
+			System.out.println(t1+space+t2+space+(t3+1));
+			System.out.println("\n");
+		}else {
+			hanoiTowers(d-1,t1,t3,t2);
+			t3++;
+			System.out.println(t1+space+t2+space+t3);
+			hanoiTowers(d-1,t2,t1,t3);
+			
+		}
+	}
+	public static void hanoiTowers(int d) {
+		String space=" ";
+		hanoiTowers(d,0,0,0);
+		System.out.println(d+space+0+space+0);
 	}
 
 }
