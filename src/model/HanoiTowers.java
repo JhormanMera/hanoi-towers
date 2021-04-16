f846ae2d80b6c14ab32f2b83d02cd6a08c448efc
JhormanMera <jhorman.mera1@correo.icesi.edu.co>
2021-04-15 19:33:17 -0500

    hanoi 3
    Hanoi problem solved, we are going to check that the output is generated correctly

---
+++ src/model/HanoiTowers.java
@@ -0,0 +1,80 @@
+package model;
+
+public class HanoiTowers {
+	private int origin;
+	private int aux;
+	private int dest;
+
+	public HanoiTowers() {
+		origin=0;
+		aux=0;
+		dest=0;
+	}
+	public void hanoiTowers(int d) {
+		hanoiTowers(d,"A","B","C");
+	}
+	
+	public void setTowers(int d) {
+		origin=d;
+		aux=0;
+		dest=0;
+	}
+	
+	private void hanoiTowers(int d, String o, String a, String des) {
+		String space=" ";	
+		if(d==1){	
+			hanoiTowers(o,des);
+			System.out.println(origin+space+aux+space+dest);
+		}else{		
+			hanoiTowers(d-1,o,des,a);
+			hanoiTowers(o,des);
+			System.out.println(origin+space+aux+space+dest);
+			hanoiTowers(d-1,a,o,des);
+		}
+	}
+	
+	private void hanoiTowers(String o, String des) {
+		switch(o) {
+		case "A":
+			origin--;
+		break;
+		case "B":
+			aux--;
+		break;	
+		case "C":
+			dest--;
+		break;
+		}
+		switch(des) {
+		case "A":
+			origin++;
+		break;
+		case "B":
+			aux++;
+		break;
+		case "C":
+			dest++;
+		break;
+		}
+	}
+
+	/*
+	public void hanoiTowers(int d) {
+		hanoiTowers(d,1,2,3);		
+	}	
+
+	private void hanoiTowers(int d, int t1, int t2, int t3) {
+		int torre1=t1;
+		int torre2=t2;
+		int torre3=t3;
+		if(d==1) {
+			System.out.println("Mover disco de torre "+t1+" a torre "+t3);		
+		}else {
+			hanoiTowers((d-1),torre1,torre3,torre2);			
+			System.out.println("Mover disco de torre "+t1+" a torre "+t3);
+			hanoiTowers((d-1),torre2,torre1,torre3);		
+		}
+	}
+	 */
+
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,67 +3,37 @@ package ui;
 
 import java.util.Scanner;
 
+import model.HanoiTowers;
+
 public class Main {
 	private static Scanner sc =new Scanner(System.in);
-	
+	private static HanoiTowers h;
+
 	public static void main(String[] args) {
+		h = new HanoiTowers();
+		start();
+	}
+	public static void start() {
 		int cases = sc.nextInt();
 		sc.nextLine();
 		int[] disks = new int[cases];
-		System.out.println("Número de casos leído");
 		int repeat=0;
 		while(repeat<cases){
 			disks[repeat]=sc.nextInt();
 			sc.nextLine();
 			repeat++;			
 		}
-		System.out.println("Terminó la lecura de número de discos ");
 		for (int i=0;i<disks.length;i++) {
-			hanoiTowers(disks[i]);
-			System.out.println("\n");
+			String space=" ";
+			System.out.println(disks[i]+space+0+space+0);
+			h.setTowers(disks[i]);
+			h.hanoiTowers(disks[i]);	
+			System.out.println("");
+
 		}
 	}
-	private static void hanoiTowers(int d, int t1, int t2, int t3) {
-		String space=" ";
-		int torre1=t1;
-		int torre2=t2;
-		int torre3=t3;
-		if(d==1) {
-			System.out.print("Entró al caso base "+"\n");
-			System.out.print("Mover disco de torre "+t1+" a torre "+t3+"\n");
-		
-		}else {
-			hanoiTowers((d-1),torre1,torre3,torre2);			
-			System.out.print("Mover disco de torre "+t1+" a torre "+t3+"\n");
-			hanoiTowers((d-1),torre2,torre1,torre3);		
-			
-		}
-	}
-	/*private static void hanoiTowers(int d, int t1, int t2, int t3) {
-		String space=" ";
-		int torre1=t1;
-		int torre2=t2;
-		int torre3=t3;
-		if(d==1){
-			System.out.print("Entró al caso base "+"\n");
-			System.out.print(torre1+space+torre2+space+torre3+"\n");		
-			System.out.print("Hpta "+"\n");
-		}else{	
-			torre1--;
-			torre3++;
-			hanoiTowers((d-1),torre1,torre3,torre2);			
-			System.out.print(torre1+space+torre2+space+torre3+"\n");
-			torre2--;
-			torre3++;
-			hanoiTowers((d-1),torre1,torre2,torre3);
-		}
-	}
-	*/
-	public static void hanoiTowers(int d) {
-		String space=" ";
-		System.out.println(d+space+0+space+0);
-		hanoiTowers(d,1,2,3);
-		
-	}
+
+
+
 
 }
